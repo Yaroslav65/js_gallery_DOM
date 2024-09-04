10f5523923c85c0ad6504593a1b41b364013b677
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-09-05 00:23:39 +0200

    make build

---
+++ index.f5421ec9.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var n=e.target.closest("a");n&&(t.src=n.href),e.preventDefault()});
+//# sourceMappingURL=index.f5421ec9.js.map
